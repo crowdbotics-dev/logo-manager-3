7c4b6567eab39d9d9b467b7a951a6046676cc5ae
Crowdbotics <engineering@crowdbotics.com>
2023-09-29 15:40:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "logo-manager-3",
+  url: "https://logo-manager-3.botics.co",
+  api: "https://logo-manager-3.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
